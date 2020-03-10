75ef71ddc9d5eec13393d5b9fd9223ff09fd6f9b
sbx_user1051 <sbx_user1051@169.254.26.173>
2020-03-10 11:27:17 +0000

    Tue Mar 10 11:27:17 UTC 2020

---
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -0,0 +1,16 @@
+﻿using BeFaster.App.Solutions.SUM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeFaster.App.Tests.Solutions.SUM
+{
+    [TestClass]
+    public class SumSolutionTest
+    {
+        [TestMethod]
+        [DataRow(1, 1)]
+        public void ComputeSum(int x, int y)
+        {
+            Assert.AreEqual(SumSolution.Sum(x, y), 2);
+        }
+    }
+}
---
+++ src/BeFaster.App.Tests/Solutions/TST/OneTest.cs
@@ -0,0 +1,14 @@
+using BeFaster.App.Solutions.TST;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeFaster.App.Tests.Solutions.TST
+{
+    [TestClass]
+    public class OneTest {
+    
+        [TestMethod]
+        public void RunApply() {
+            Assert.AreEqual(One.apply(), 1);
+        }
+    }
+}
