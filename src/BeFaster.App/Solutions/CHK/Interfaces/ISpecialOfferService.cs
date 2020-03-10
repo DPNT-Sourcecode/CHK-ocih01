15c869fa6e34ecb9763e8a44c7c345f59ff5880a
sbx_user1051 <sbx_user1051@169.254.26.173>
2020-03-10 20:22:05 +0000

    Tue Mar 10 20:22:05 UTC 2020

---
+++ src/BeFaster.App/Solutions/CHK/Interfaces/ISpecialOfferService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK.Interfaces
+{
+    public interface ISpecialOfferService
+    {
+        int GetDiscountedPrice(char productId, int cartItemQuantity, int actualProductPrice);
+        IDictionary<char, int> ApplyBuyOneProductGetAnotherProductFreeOffer(IDictionary<char, int> skuCounts);
+    }
+}
--- src/BeFaster.App/Solutions/CHK/Services/SpecialOfferService.cs
+++ src/BeFaster.App/Solutions/CHK/Services/SpecialOfferService.cs
@@ -1,6 +1,7 @@
 ﻿using BeFaster.App.Solutions.CHK.Interfaces;
 using BeFaster.App.Solutions.CHK.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BeFaster.App.Solutions.CHK.Services
 {
@@ -25,5 +26,31 @@ namespace BeFaster.App.Solutions.CHK.Services
             }
             return discountedPrice;
         }
+
+
+        public static IDictionary<char, int> ApplyBuyOneProductGetAnotherProductFreeOffer(IDictionary<char, int> skuCounts)
+        {
+            var offers = SpecialOffers.Where(x => x.Value.Any(y => y.GetType().Equals(typeof(BuyOneGetAnotherFree))))
+                .ToDictionary(s => s.Key, s => s.Value.Where(z => z.OfferType == Enums.SpecialOfferType.BuyOneGetAnotherFree).ToList());
+
+            foreach (var offer in offers)
+            {
+                if (skuCounts.Keys.Contains(offer.Key))
+                {
+                    foreach (BuyOneGetAnotherFree buyOneGetOneOffer in offer.Value)
+                    {
+                        if (skuCounts[offer.Key] >= buyOneGetOneOffer.ItemQuantity && skuCounts.Keys.Contains(buyOneGetOneOffer.FreeItemId))
+                        {
+                            var numberOFItemsToReduce = (skuCounts[offer.Key] / buyOneGetOneOffer.ItemQuantity) * buyOneGetOneOffer.FreeItemQuantity;
+                            int itemCountAfterReduction = skuCounts[buyOneGetOneOffer.FreeItemId] - numberOFItemsToReduce;
+                            skuCounts[buyOneGetOneOffer.FreeItemId] = itemCountAfterReduction > 0 ? itemCountAfterReduction : 0;
+                        }
+                    }
+                }
+            }
+
+            return skuCounts;
+        }
     }
 }
+
