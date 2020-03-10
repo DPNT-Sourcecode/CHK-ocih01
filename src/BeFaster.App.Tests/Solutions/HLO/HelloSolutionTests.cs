3a9251aa677d3e59eac9891b4a69c4bebb33affd
sbx_user1051 <sbx_user1051@169.254.26.173>
2020-03-10 11:50:19 +0000

    Tue Mar 10 11:50:19 UTC 2020

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTests.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeFaster.App.Solutions.HLO;
 
 namespace BeFaster.App.Tests.Solutions.HLO
 {
-    class HelloSolutionTests
+    [TestClass]
+    public class HelloSolutionTests
     {
+        [TestMethod]
+        [DataRow("")]
+        [DataRow(null)]
+        [DataRow("-1")]
+        [DataRow("jack")]
+        [DataRow("j@-/ck")]
+        public void Hello_should_return_HelloWorld(string s)
+        {
+            Assert.AreEqual("Hello, World!", HelloSolution.Hello(s));
+        }
     }
 }
+
