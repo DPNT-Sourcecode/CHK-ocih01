0d4cbe72e62038fed3bebb0bf6e668a89d229a11
sbx_user1051 <sbx_user1051@169.254.26.173>
2020-03-10 20:19:05 +0000

    Tue Mar 10 20:19:05 UTC 2020

---
+++ src/BeFaster.App/Solutions/CHK/Services/SpecialOfferService.cs
@@ -0,0 +1,29 @@
+﻿using BeFaster.App.Solutions.CHK.Interfaces;
+using BeFaster.App.Solutions.CHK.Models;
+using System.Collections.Generic;
+
+namespace BeFaster.App.Solutions.CHK.Services
+{
+    public class SpecialOfferService
+    {
+        public static readonly IDictionary<char, IList<ISpecialOffer>> SpecialOffers = new Dictionary<char, IList<ISpecialOffer>>();
+
+        public static int GetDiscountedPrice(char productId, int cartItemQuantity, int actualProductPrice)
+        {
+            int discountedPrice = 0;
+            var specialOffers = SpecialOffers[productId];
+            bool isFirstPriceCalculation = true;
+
+            foreach (var offer in specialOffers)
+            {
+                if (offer is BuyMultipleForPriceReduction multiplePriceReductionOffer)
+                {
+                    int calculatedDiscountedPrice = multiplePriceReductionOffer.GetDiscountedPrice(productId, cartItemQuantity, actualProductPrice);
+                    discountedPrice = isFirstPriceCalculation || calculatedDiscountedPrice < discountedPrice ? calculatedDiscountedPrice : discountedPrice;
+                    isFirstPriceCalculation = false;
+                }
+            }
+            return discountedPrice;
+        }
+    }
+}
