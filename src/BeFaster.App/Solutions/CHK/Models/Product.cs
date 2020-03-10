1a49d94dea078bda7642180a8f7156b344d9af28
sbx_user1051 <sbx_user1051@169.254.26.173>
2020-03-10 23:25:57 +0000

    Tue Mar 10 23:25:57 UTC 2020

--- src/BeFaster.App/Solutions/CHK/Models/Product.cs
+++ src/BeFaster.App/Solutions/CHK/Models/Product.cs
@@ -1,8 +1,13 @@
-﻿namespace BeFaster.App.Solutions.CHK.Models
+﻿using BeFaster.App.Solutions.CHK.Interfaces;
+using System.Collections.Generic;
+
+namespace BeFaster.App.Solutions.CHK.Models
 {
     public class Product
     {
         public char Id { get; set; }
         public int Price { get; set; }
+        public IList<SpecialOffer> SpecialOffers{get; set;}
     }
 }
+
