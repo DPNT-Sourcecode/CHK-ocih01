a8c51bc2a1220ae36ce9d9feb3844e1e0b51c5ef
sbx_user1051 <sbx_user1051@169.254.26.173>
2020-03-10 17:31:03 +0000

    Tue Mar 10 17:31:03 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/CheckOutSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckOutSolutionTests.cs
@@ -1,6 +1,5 @@
 ﻿using BeFaster.App.Solutions.CHK;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BeFaster.App.Solutions.CHK.Models;
 
 namespace BeFaster.App.Tests.Solutions.CHK
 {
@@ -30,23 +29,14 @@ namespace BeFaster.App.Tests.Solutions.CHK
         }
 
         [TestMethod]
-        public void ComputePrice_Should_Return_InvalidInput_Given_Empty_ProductsList()
-        {
-            CheckoutSolution.Products.Clear();
-            Assert.AreEqual(invalidInput, CheckoutSolution.ComputePrice("Z"));
-        }
-
-        [TestMethod]
         public void ComputePrice_Should_Return_InvalidInput_Given_SKU_That_Does_Not_Exist_In_ProductsList()
         {
-            AddProducts();
             Assert.AreEqual(invalidInput, CheckoutSolution.ComputePrice("Z"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_InvalidInput_Given_SKU_That_Does_Not_Exist_In_ProductsList_With_Other_Valid_SKUs()
         {
-            AddProducts();
             Assert.AreEqual(invalidInput, CheckoutSolution.ComputePrice("AZC"));
         }
 
@@ -57,35 +47,24 @@ namespace BeFaster.App.Tests.Solutions.CHK
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Single_SKU()
         {
-            AddProducts();
             Assert.AreEqual(50, CheckoutSolution.ComputePrice("A"));
-        }
-
-        [TestMethod]
-        public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Single_SKU()
-        {
-            AddProducts();
-            Assert.AreEqual(50, CheckoutSolution.ComputePrice("B"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_Distinct_SKUs()
         {
-            AddProducts();
             Assert.AreEqual(35, CheckoutSolution.ComputePrice("CD"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_Repeated_SKUs()
         {
-            AddProducts();
             Assert.AreEqual(40, CheckoutSolution.ComputePrice("CC"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_Repeated_And_NonRepeated_SKUs()
         {
-            AddProducts();
             Assert.AreEqual(120, CheckoutSolution.ComputePrice("CDCDA"));
         }
 
@@ -96,48 +75,36 @@ namespace BeFaster.App.Tests.Solutions.CHK
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Single_SKU_With_SpecialOffer()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(50, CheckoutSolution.ComputePrice("A"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Single_SKU_That_Satisfies_SpecialOffer()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(130, CheckoutSolution.ComputePrice("AAA"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Single_SKU_That_has_Combination_That_Satisfies_SpecialOffer()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(180, CheckoutSolution.ComputePrice("AAAA"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_SKU_That_has_One_Combination_That_Satisfies_SpecialOffer()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(150, CheckoutSolution.ComputePrice("ACAA"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_SKU_That_has_SingleCombination_That_Satisfies_SpecialOffer()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(175, CheckoutSolution.ComputePrice("ABABA"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_SKU_That_has_Multiple_Combination_That_Satisfies_SpecialOffer()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(220, CheckoutSolution.ComputePrice("ABABABB"));
         }
 
@@ -145,22 +112,19 @@ namespace BeFaster.App.Tests.Solutions.CHK
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_SKU_That_Satisfies_Single_SpecialOffer_Along_With_Regular_SKU()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(215, CheckoutSolution.ComputePrice("ABABACC"));
         }
 
         [TestMethod]
         public void ComputePrice_Should_Return_Correct_TotalPrice_Given_Multiple_SKU_That_Satisfies_Multiple_SpecialOffers_Along_With_Regular_SKU()
         {
-            AddProducts();
-            AddSpecialOffers();
             Assert.AreEqual(255, CheckoutSolution.ComputePrice("ABABABBCD"));
         }
 
         #endregion
     }
 }
+
 
 
 
