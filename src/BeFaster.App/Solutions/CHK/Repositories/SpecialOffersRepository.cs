ffacef2b9958b025d5a34d543e79bdf07fb63d24
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 09:15:21 +0000

    Wed Mar 11 09:15:21 UTC 2020

--- src/BeFaster.App/Solutions/CHK/Repositories/SpecialOffersRepository.cs
+++ src/BeFaster.App/Solutions/CHK/Repositories/SpecialOffersRepository.cs
@@ -21,9 +21,93 @@ namespace BeFaster.App.Solutions.CHK.Repositories
                     ProductId = 'A',
                     ItemQuantity = 5,
                     SpecialPrice = 200
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'B',
+                    ItemQuantity = 2,
+                    SpecialPrice = 45
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'H',
+                    ItemQuantity = 5,
+                    SpecialPrice = 45
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'H',
+                    ItemQuantity = 10,
+                    SpecialPrice = 80
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'K',
+                    ItemQuantity = 2,
+                    SpecialPrice = 120
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'P',
+                    ItemQuantity = 5,
+                    SpecialPrice = 200
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'Q',
+                    ItemQuantity = 3,
+                    SpecialPrice = 80
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'V',
+                    ItemQuantity = 2,
+                    SpecialPrice = 90
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'V',
+                    ItemQuantity = 3,
+                    SpecialPrice = 130
+                },
+                new BuyOneGetAnotherFreeOffer
+                {
+                    ProductId = 'E',
+                    FreeItemId = 'B',
+                    ItemQuantity = 2,
+                    FreeItemQuantity = 1
+                },
+                new BuyOneGetAnotherFreeOffer
+                {
+                    ProductId = 'F',
+                    FreeItemId = 'F',
+                    ItemQuantity = 2,
+                    FreeItemQuantity = 1
+                },
+                new BuyOneGetAnotherFreeOffer
+                {
+                    ProductId = 'N',
+                    FreeItemId = 'M',
+                    ItemQuantity = 3,
+                    FreeItemQuantity = 1
+                },
+                new BuyOneGetAnotherFreeOffer
+                {
+                    ProductId = 'R',
+                    FreeItemId = 'Q',
+                    ItemQuantity = 3,
+                    FreeItemQuantity = 1
+                },
+                new BuyOneGetAnotherFreeOffer
+                {
+                    ProductId = 'U',
+                    FreeItemId = 'U',
+                    ItemQuantity = 3,
+                    FreeItemQuantity = 1
                 }
             };
         }
     }
 }
+
 
