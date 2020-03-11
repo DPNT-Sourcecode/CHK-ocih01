fc27cc34eeccd997498706f2675bb4fb2d854e0a
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 09:21:21 +0000

    Wed Mar 11 09:21:21 UTC 2020

--- src/BeFaster.App/Solutions/CHK/Interfaces/ISpecialOffersRepository.cs
+++ src/BeFaster.App/Solutions/CHK/Interfaces/ISpecialOffersRepository.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using BeFaster.App.Solutions.CHK.Enums;
+using System;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions.CHK.Interfaces
 {
     public interface ISpecialOffersRepository
     {
         IList<ISpecialOffer> GetAllSpecialOffers();
+        IList<ISpecialOffer> GetSpecialOffersByType(SpecialOfferType specialOfferType );
     }
 }
+
 
