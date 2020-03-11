f741caad275401a7156a9f7a4858426c48875811
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 09:27:21 +0000

    Wed Mar 11 09:27:21 UTC 2020

--- src/BeFaster.App/Solutions/CHK/Repositories/SpecialOffersRepository.cs
+++ src/BeFaster.App/Solutions/CHK/Repositories/SpecialOffersRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using BeFaster.App.Solutions.CHK.Enums;
 using BeFaster.App.Solutions.CHK.Interfaces;
 using BeFaster.App.Solutions.CHK.Models;
 
@@ -21,76 +23,88 @@ namespace BeFaster.App.Solutions.CHK.Repositories
                 {
                     ProductId = 'A',
                     ItemQuantity = 5,
-                    SpecialPrice = 200
+                    SpecialPrice = 200,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'B',
                     ItemQuantity = 2,
-                    SpecialPrice = 45
+                    SpecialPrice = 45,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'H',
                     ItemQuantity = 5,
-                    SpecialPrice = 45
+                    SpecialPrice = 45,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'H',
                     ItemQuantity = 10,
-                    SpecialPrice = 80
+                    SpecialPrice = 80,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'K',
                     ItemQuantity = 2,
-                    SpecialPrice = 120
+                    SpecialPrice = 120,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'P',
                     ItemQuantity = 5,
-                    SpecialPrice = 200
+                    SpecialPrice = 200,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'Q',
                     ItemQuantity = 3,
-                    SpecialPrice = 80
+                    SpecialPrice = 80,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'V',
                     ItemQuantity = 2,
-                    SpecialPrice = 90
+                    SpecialPrice = 90,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyMultipleOfSameForPriceReductionOffer
                 {
                     ProductId = 'V',
                     ItemQuantity = 3,
-                    SpecialPrice = 130
+                    SpecialPrice = 130,
+                    OfferType = Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
                 },
                 new BuyOneGetAnotherFreeOffer
                 {
                     ProductId = 'E',
                     FreeItemId = 'B',
                     ItemQuantity = 2,
-                    FreeItemQuantity = 1
+                    FreeItemQuantity = 1,
+                    OfferType = Enums.SpecialOfferType.BuyOneGetAnotherFree
                 },
                 new BuyOneGetAnotherFreeOffer
                 {
                     ProductId = 'F',
                     FreeItemId = 'F',
                     ItemQuantity = 2,
-                    FreeItemQuantity = 1
+                    FreeItemQuantity = 1,
+                    OfferType = Enums.SpecialOfferType.BuyOneGetAnotherFree
                 },
                 new BuyOneGetAnotherFreeOffer
                 {
                     ProductId = 'N',
                     FreeItemId = 'M',
                     ItemQuantity = 3,
-                    FreeItemQuantity = 1
+                    FreeItemQuantity = 1,
+                    OfferType = Enums.SpecialOfferType.BuyOneGetAnotherFree
                 },
                 new BuyOneGetAnotherFreeOffer
                 {
@@ -104,7 +118,8 @@ namespace BeFaster.App.Solutions.CHK.Repositories
                     ProductId = 'U',
                     FreeItemId = 'U',
                     ItemQuantity = 3,
-                    FreeItemQuantity = 1
+                    FreeItemQuantity = 1,
+                    OfferType = Enums.SpecialOfferType.BuyOneGetAnotherFree
                 },
                 new BuyInBulkFromAGroupForPriceReductionOffer
                 {
@@ -113,12 +128,19 @@ namespace BeFaster.App.Solutions.CHK.Repositories
                         'S', 'T', 'X', 'Y', 'Z'
                     },
                     ItemQuantity = 3,
-                    SpecialPrice = 45
+                    SpecialPrice = 45,
+                    OfferType = Enums.SpecialOfferType.BuyInBulkFromAGroupForPriceReductionOffer
                 }
             };
         }
+
+        public IList<ISpecialOffer> GetSpecialOffersByType(SpecialOfferType specialOfferType)
+        {
+            return GetAllSpecialOffers().Where(x => x.OfferType == specialOfferType).ToList();
+        }
     }
 }
+
 
 
 
