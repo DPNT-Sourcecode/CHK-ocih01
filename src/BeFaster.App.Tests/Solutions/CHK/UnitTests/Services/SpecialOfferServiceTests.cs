ea0def56ce18218eee72ed983914136b98d68866
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 11:52:25 +0000

    Wed Mar 11 11:52:25 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Services/SpecialOfferServiceTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Services/SpecialOfferServiceTests.cs
@@ -121,6 +121,50 @@ namespace BeFaster.App.Tests.Solutions.CHK.UnitTests.Services
             mockSpecialOffersRepository.Verify(x => x.GetSpecialOffersByType<BuyOneGetAnotherFreeOffer>(), Times.Once);
         }
 
+        [TestMethod]
+        public void ApplyBuyOneProductGetAnotherProductFreeOffer_Should_Return_Correct_Counts_Given_One_Matching_SKU_Item()
+        {
+            mockSpecialOffersRepository.Setup(x => x.GetSpecialOffersByType<BuyOneGetAnotherFreeOffer>()).Returns(GetBuyOneGetAnotherFreeOffers()).Verifiable();
+
+            var skuCounts = new Dictionary<char, int>(1);
+            skuCounts.Add('E', 2);
+            skuCounts.Add('B', 1);
+
+            var actualSkuCounts = specialOfferService.ApplyBuyOneProductGetAnotherProductFreeOffer(skuCounts);
+
+            Assert.AreEqual(0, actualSkuCounts['B']);
+            mockSpecialOffersRepository.Verify(x => x.GetSpecialOffersByType<BuyOneGetAnotherFreeOffer>(), Times.Once);
+        }
+
+        [TestMethod]
+        public void ApplyBuyOneProductGetAnotherProductFreeOffer_Should_Return_Correct_Counts_Given_Multiple_Matching_SKU_Item()
+        {
+            mockSpecialOffersRepository.Setup(x => x.GetSpecialOffersByType<BuyOneGetAnotherFreeOffer>()).Returns(GetBuyOneGetAnotherFreeOffers()).Verifiable();
+
+            var skuCounts = new Dictionary<char, int>(1);
+            skuCounts.Add('E', 4);
+            skuCounts.Add('B', 1);
+
+            var actualSkuCounts = specialOfferService.ApplyBuyOneProductGetAnotherProductFreeOffer(skuCounts);
+
+            Assert.AreEqual(0, actualSkuCounts['B']);
+            mockSpecialOffersRepository.Verify(x => x.GetSpecialOffersByType<BuyOneGetAnotherFreeOffer>(), Times.Once);
+        }
+
+        [TestMethod]
+        public void ApplyBuyOneProductGetAnotherProductFreeOffer_Should_Return_Correct_Counts_Given_No_Matching_SKU_Item()
+        {
+            mockSpecialOffersRepository.Setup(x => x.GetSpecialOffersByType<BuyOneGetAnotherFreeOffer>()).Returns(GetBuyOneGetAnotherFreeOffers()).Verifiable();
+
+            var skuCounts = new Dictionary<char, int>(1);
+            skuCounts.Add('E', 2);
+
+            var actualSkuCounts = specialOfferService.ApplyBuyOneProductGetAnotherProductFreeOffer(skuCounts);
+
+            Assert.AreEqual(skuCounts, actualSkuCounts);
+            mockSpecialOffersRepository.Verify(x => x.GetSpecialOffersByType<BuyOneGetAnotherFreeOffer>(), Times.Once);
+        }
+
         #endregion
 
         private static List<BuyMultipleOfSameForPriceReductionOffer> GetMultiBuyOffers()
@@ -166,6 +210,7 @@ namespace BeFaster.App.Tests.Solutions.CHK.UnitTests.Services
         }
     }
 }
+
 
 
 
