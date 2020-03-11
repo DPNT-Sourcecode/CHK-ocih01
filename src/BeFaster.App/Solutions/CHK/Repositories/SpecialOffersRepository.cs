6e3f363ddae84a8bcbf1b5456a31b8b1cbb30e24
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 09:09:21 +0000

    Wed Mar 11 09:09:21 UTC 2020

--- src/BeFaster.App/Solutions/CHK/Repositories/SpecialOffersRepository.cs
+++ src/BeFaster.App/Solutions/CHK/Repositories/SpecialOffersRepository.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using BeFaster.App.Solutions.CHK.Interfaces;
+using BeFaster.App.Solutions.CHK.Models;
 
 namespace BeFaster.App.Solutions.CHK.Repositories
 {
-    class SpecialOffersRepository
+    public class SpecialOffersRepository : ISpecialOffersRepository
     {
+        public IList<ISpecialOffer> GetAllSpecialOffers()
+        {
+            return new List<ISpecialOffer>
+            {
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'A',
+                    ItemQuantity = 3,
+                    SpecialPrice = 130
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'A',
+                    ItemQuantity = 5,
+                    SpecialPrice = 200
+                }
+            };
+        }
     }
 }
+
