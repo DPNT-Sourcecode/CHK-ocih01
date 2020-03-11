066d4d11d085ab24c547ca2984ac965d320504eb
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 08:54:21 +0000

    Wed Mar 11 08:54:21 UTC 2020

--- src/BeFaster.App/Solutions/CHK/Repositories/ProductsRepository.cs
+++ src/BeFaster.App/Solutions/CHK/Repositories/ProductsRepository.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using BeFaster.App.Solutions.CHK.Interfaces;
+using BeFaster.App.Solutions.CHK.Models;
 
 namespace BeFaster.App.Solutions.CHK.Repositories
 {
-    class ProductsRepository
+    public class ProductsRepository : IProductsRepository
     {
+        public IDictionary<char, Product> GetProducts()
+        {
+            IDictionary<char, Product> products = new Dictionary<char, Product>(26);
+            IList<int> priceList = GetPriceList();
+            int i = 0;
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                products.Add(c, new Product
+                {
+                    Id = c,
+                    Price = priceList[i];
+            });
+            i++;
+        }
+
+
+        private IList<int> GetPriceList() => new List<int>{
+            50, 30, 20, 15, 40, 10, 20, 10, 35, 60, 70, 90, 15, 40, 10, 50, 30,50, 20, 20, 40, 50, 20, 17, 20, 21
+        };
     }
+
 }
+
