67388ca39d5078ce31db0a8cf853e907d83d9b0d
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 11:28:24 +0000

    Wed Mar 11 11:28:24 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Services/SpecialOfferServiceTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Services/SpecialOfferServiceTests.cs
@@ -1,7 +1,9 @@
 ﻿using BeFaster.App.Solutions.CHK.Interfaces;
+using BeFaster.App.Solutions.CHK.Models;
 using BeFaster.App.Solutions.CHK.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Tests.Solutions.CHK.UnitTests.Services
 {
@@ -9,12 +11,41 @@ namespace BeFaster.App.Tests.Solutions.CHK.UnitTests.Services
     public class SpecialOfferServiceTests
     {
         Mock<ISpecialOffersRepository> mockSpecialOffersRepository = new Mock<ISpecialOffersRepository>();
+        SpecialOfferService specialOfferService;
+
+        public SpecialOfferServiceTests()
+        {
+            specialOfferService = new SpecialOfferService(mockSpecialOffersRepository.Object);
+        }
 
         [TestMethod]
         public void GetDiscountedPrice_Should_Return_CorrectPrice_Given_A_MultiBuyOffer()
         {
-            var specialOfferService = new SpecialOfferService(mockSpecialOffersRepository.Object);
+            List<BuyMultipleOfSameForPriceReductionOffer> multiBuyOffers = GetMultiBuyOffers();
+            mockSpecialOffersRepository.Setup(x => x.GetSpecialOffersByType<BuyMultipleOfSameForPriceReductionOffer>()).Returns(multiBuyOffers).Verifiable();
+        }
+
+        private static List<BuyMultipleOfSameForPriceReductionOffer> GetMultiBuyOffers()
+        {
+            return new List<BuyMultipleOfSameForPriceReductionOffer>
+            {
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'A',
+                    ItemQuantity = 3,
+                    SpecialPrice = 130,
+                    OfferType = App.Solutions.CHK.Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
+                },
+                new BuyMultipleOfSameForPriceReductionOffer
+                {
+                    ProductId = 'A',
+                    ItemQuantity = 5,
+                    SpecialPrice = 200,
+                    OfferType = App.Solutions.CHK.Enums.SpecialOfferType.BuyMultipleOfSameForPriceReduction
+                }
+            };
         }
     }
 }
+
 
