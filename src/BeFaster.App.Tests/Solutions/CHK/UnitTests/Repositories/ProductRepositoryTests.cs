102626e93254470ba8d41a5f20981f4eb31382cf
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 11:01:25 +0000

    Wed Mar 11 11:01:25 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Repositories/ProductRepositoryTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Repositories/ProductRepositoryTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BeFaster.App.Solutions.CHK.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace BeFaster.App.Tests.Solutions.CHK.UnitTests
+namespace BeFaster.App.Tests.Solutions.CHK.UnitTests.Repositories
 {
-    class ProductRepositoryTests
+    [TestClass]
+    public class ProductRepositoryTests
     {
+        [TestMethod]
+        public void GetAllProducts_Should_Return_Correct_Number_Of_Products()
+        {
+            var products = new ProductsRepository().GetAllProducts();
+
+            Assert.AreEqual(26, products.Count);
+        }
+
+        [TestMethod]
+        public void GetAllProducts_Should_Return_Correct_Number_Of_Product_Keys()
+        {
+            var products = new ProductsRepository().GetAllProducts();
+
+            Assert.AreEqual(26, products.Count);
+        }
     }
 }
+
