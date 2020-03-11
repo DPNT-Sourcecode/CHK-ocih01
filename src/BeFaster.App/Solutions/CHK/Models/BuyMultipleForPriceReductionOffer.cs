78e5b960626fa2c6da5539ab85dbc0198c5abb91
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 12:16:24 +0000

    Wed Mar 11 12:16:24 UTC 2020

--- src/BeFaster.App/Solutions/CHK/Models/BuyMultipleForPriceReductionOffer.cs
+++ src/BeFaster.App/Solutions/CHK/Models/BuyMultipleForPriceReductionOffer.cs
@@ -4,7 +4,10 @@ namespace BeFaster.App.Solutions.CHK.Models
 {
     public class BuyMultipleOfSameForPriceReductionOffer : SpecialOffer
     {
-        public IList<char> ProductId { get; set; }
+        public char ProductId { get; set; }
+        public bool IsGroupingAllowed { get; set; }
+        public IList<char> CombinationProducts { get; set; }
+
         public int SpecialPrice { get; set; }
 
         public int GetDiscountedPrice(int cartItemQuantity, int actualProductPrice)
@@ -13,3 +16,4 @@ namespace BeFaster.App.Solutions.CHK.Models
         }
     }
 }
+
