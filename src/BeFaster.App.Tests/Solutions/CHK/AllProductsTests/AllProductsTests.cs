9360989b246226de649c8f87b337c4ad7a4ebf13
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 02:52:19 +0000

    Wed Mar 11 02:52:19 UTC 2020

---
+++ src/BeFaster.App.Tests/Solutions/CHK/AllProductsTests/AllProductsTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeFaster.App.Tests.Solutions.CHK.AllProductsTests
+{
+    [TestClass]
+    public class AllProductsTests
+    {
+        private static IDictionary<char, Product> products = GetProducts();
+    }
+}
