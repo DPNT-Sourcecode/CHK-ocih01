de729ebbe0d3104fc89327139e8b81cd3f5bb8b8
sbx_user1051 <sbx_user1051@169.254.153.45>
2020-03-11 11:25:24 +0000

    Wed Mar 11 11:25:24 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Services/SpecialOfferServiceTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/UnitTests/Services/SpecialOfferServiceTests.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BeFaster.App.Solutions.CHK.Interfaces;
+using BeFaster.App.Solutions.CHK.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace BeFaster.App.Tests.Solutions.CHK.UnitTests.Services
 {
     [TestClass]
     public class SpecialOfferServiceTests
     {
+        Mock<ISpecialOffersRepository> mockSpecialOffersRepository = new Mock<ISpecialOffersRepository>();
+
+        [TestMethod]
+        public void GetDiscountedPrice_Should_Return_CorrectPrice_Given_A_MultiBuyOffer()
+        {
+            var specialOfferService = new SpecialOfferService(mockSpecialOffersRepository.Object);
+        }
     }
 }
+
